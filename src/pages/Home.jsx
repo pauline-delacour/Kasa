7d11924afed411a95fe96867612d773d768139c4
Pauline Delacour <pauline.delacour33@gmail.com>
2024-09-10 11:27:01 +0200

    Creation des cards avec les élements du fichier json

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 import imageBanner from '../assets/imageBanner.png';
 import Card from "../components/Cards";
-import "../styles/Home/Cards.scss"; 
-
+import "../styles/Home/Home.scss";
+import logements from "../data/logements.json";
 
 const Home = () => {
   return (
     <><div className="image-banner">
-      <img src={imageBanner} alt="plage qui donne sur une falaise" />
-      <span>Chez vous, partout et ailleurs</span>
+      <img src={imageBanner} alt="plage qui donne sur une falaise" className="imageBanner"/>
+      <span className="textBanner">Chez vous, partout et ailleurs</span>
     </div>
     <div className="card-content">
         <div className="cards">
-         <Card/>
-         <Card/>
-         <Card/>
-         <Card/>
-         <Card/>
-         <Card/>
+        {logements.map((logement) => (
+          <Card
+          key={logement.id}
+          title={logement.title}
+          cover={logement.cover}/>
+        ))}
         </div>
       </div></>
   );
