2d0ced832590455442f65fdff98cbf099c87d643
Pauline Delacour <pauline.delacour33@gmail.com>
2024-09-10 11:27:57 +0200

    Page " à propos"

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,29 @@
 import React from "react";
+import '../styles/About/About.scss';
+import imageAbout from '../assets/image-about.png';
+import Collapse from "../components/Collapse";
 
 const About = () => {
   return (
-    <div>
-      <h2>À propos</h2>
-      <p>Ceci est la page à propos</p>
+    <div className="About">
+      <img src={imageAbout} alt="montage" className="imageAbout" />
+      <div className="content-about">
+       <Collapse
+       title={<span>Fiabilité</span>}
+       content={<p>Ceci est le texte du composant collapse</p>}/>
+       <Collapse
+       title={<span>Respect</span>}
+       content={<p>Ceci est le texte du composant collapse</p>}/>
+        <Collapse
+       title={<span>Service</span>}
+       content={<p>Ceci est le texte du composant collapse</p>}/>
+         <Collapse
+       title={<span>Sécurité</span>}
+       content={<p>Ceci est le texte du composant collapse</p>}/>
+      </div>  
     </div>
   );
 }
+
+
 export default About;
