62c759d7e85dd36725db31af730567f449c17045
Pauline Delacour <pauline.delacour33@gmail.com>
2024-09-16 09:41:43 +0200

    composant Cards => ajout de Link et en parametre l'id

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,17 +1,20 @@
 import React from "react";
-import "../styles/Home/Cards.scss";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-const Card = ({title, cover,}) => {
-    return(
-        <div className="card">
-          <img src={cover} alt={title} className="card-cover"/>
-          <span className="card-title">{title}</span>
-        </div>
-    );
-}
+const Card = ({ id, title, cover }) => {
+  return (
+    <div className="card">
+      <Link to={`/logement/${id}`}>
+        <img src={cover} alt={title} className="card-cover" />
+        <span className="card-title">{title}</span>
+      </Link>
+    </div>
+  );
+};
 Card.propTypes = {
+  id: PropTypes.string,
   title: PropTypes.string,
   cover: PropTypes.string,
-}
+};
 export default Card;
