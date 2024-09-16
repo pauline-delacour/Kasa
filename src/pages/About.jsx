f6e9237eb20b8d43265497572060068e0fa0c630
Pauline Delacour <pauline.delacour33@gmail.com>
2024-09-16 09:47:53 +0200

    Ajout des composants BannerImage et Collapse

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,25 +1,27 @@
 import React from "react";
-import '../styles/About/About.scss';
 import imageAbout from '../assets/image-about.png';
 import Collapse from "../components/Collapse";
+import BannerImage from "../components/Banner";
+import AboutData from "../data/about.json";
+
 
 const About = () => {
   return (
     <div className="About">
-      <img src={imageAbout} alt="montage" className="imageAbout" />
+      <BannerImage
+       image={imageAbout}
+       allText="Montagne "
+       text=""
+       hasOverlay={false}
+       />
       <div className="content-about">
-       <Collapse
-       title={<span>Fiabilité</span>}
-       content={<p>Ceci est le texte du composant collapse</p>}/>
-       <Collapse
-       title={<span>Respect</span>}
-       content={<p>Ceci est le texte du composant collapse</p>}/>
-        <Collapse
-       title={<span>Service</span>}
-       content={<p>Ceci est le texte du composant collapse</p>}/>
-         <Collapse
-       title={<span>Sécurité</span>}
-       content={<p>Ceci est le texte du composant collapse</p>}/>
+          {AboutData.map((article) => (
+            <Collapse
+              key={article.id}
+              title={article.title}
+              content={article.text}
+            />
+          ))}
       </div>  
     </div>
   );
