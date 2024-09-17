1a077e5289bc6131723049bf18889bea5f82bdce
Pauline Delacour <pauline.delacour33@gmail.com>
2024-09-17 15:56:24 +0200

    utilisation de useLocation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import logo from '../assets/LOGO.png';
 
 const Header = () => {
+  const location = useLocation()
+  // récupére le chemin de la page courante
+
   return (
     <nav className="header">
       <img src={logo} alt="Logo" className="logo-kasa"/>
 
       <div className="nav-link">
-        <Link to="/">Accueil</Link>
-        <Link to="/about">A propos</Link>
+        <Link to="/" className={location.pathname === "/" ? "active" : ""}>Accueil</Link>
+        <Link to="/about" className={location.pathname === "/about" ? "active" : ""}>A propos</Link>
       </div>
     </nav>
   );          
